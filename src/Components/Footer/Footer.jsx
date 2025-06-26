e1ff7fe2fe383da0410376745fc1875a0883a06c
AhmaduNo1 <programmerone37@gmail.com>
2025-06-26 22:59:57 -0100

    Fix: correct Contact.css import path

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer.css";
+import "./footer.css";
 import { FaPhoneAlt } from "react-icons/fa";
 import { AiFillTikTok } from "react-icons/ai";
 import { FaLinkedin } from "react-icons/fa";
